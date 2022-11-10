9987ad57a33ee80bf48b7c7a31c6c32690d65e87
Raihan Aria <raihan.aria.ft21@mail.umy.ac.id>
2022-11-10 19:11:17 +0700

    create method addNode

--- singly_linked_list/Program.cs
+++ singly_linked_list/Program.cs
@@ -19,6 +19,49 @@ namespace singly_linked_list
         {
             START = null;
         }
+        //Method untuk menambahkan sebuah node kedalam list
+        public void addNode()
+        {
+            int nim;
+            string nm;
+            Console.Write("\nMasukkan nomor Mahasiswa: ");
+            nim = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nMasukkan nama Mahasiswa: ");
+            nm = Console.ReadLine();
+            Node nodeBaru = new Node();
+            nodeBaru.noMhs = nim;
+            nodeBaru.nama = nm;
+
+            //Node ditambahkan sebagai nodebaru
+            if (START == null || nim <= START.noMhs)
+            {
+                if ((START != null) && (nim == START.noMhs))
+                {
+                    Console.WriteLine("\nNomer mahasiswa sama tidak diijinkan\n");
+                    return;
+                }
+                nodeBaru.next = START;
+                START = nodeBaru;
+                return;
+            }
+            //Menemukan lokasi node baru didalam list
+            Node previous, current;
+            previous = START;
+            current = START;
+            while ((current != null) && (nim >= current.noMhs))
+            {
+                if (nim == current.noMhs)
+                {
+                    Console.WriteLine("\nNomer mahasiswa sama tidak diijinkan\n");
+                    return;
+                }
+                previous = current;
+                current = current.next;
+            }
+            //Node baru akan ditempatkan diantara previous dan current
+            nodeBaru.next = current;
+            previous.next = nodeBaru;
+        }
     }
     
     class Program
